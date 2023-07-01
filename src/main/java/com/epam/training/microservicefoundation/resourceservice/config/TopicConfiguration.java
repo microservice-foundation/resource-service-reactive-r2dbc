392966b236252d3876ffd2fe04e35405b5f9e13a
yusuf-murodov-epam <yusuf.murodov@alertinnovation.com>
2023-07-01 16:25:53 +0500

    Change Pair dependency

--- src/main/java/com/epam/training/microservicefoundation/resourceservice/config/TopicConfiguration.java
+++ src/main/java/com/epam/training/microservicefoundation/resourceservice/config/TopicConfiguration.java
@@ -11,8 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.util.Pair;
 import org.springframework.kafka.config.TopicBuilder;
-import software.amazon.awssdk.utils.Pair;
 
 @Configuration
 @RefreshScope
